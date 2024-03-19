4b444274c448f283a202eb00dc6067650880b52c
Nicolas Fandiño <nicolasfandi@hotmail.com>
2024-03-19 10:14:40 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  return <div>
+      <h1>Ingrese su nombre</h1> 
+      <input type="text" name="Nombre" id="NId"/>
+      <h2>Bienvenido {nombre}</h2>
+    </div>
+}
+export default App; 
+
